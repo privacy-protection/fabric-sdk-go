08f5029ddd9d24b649d90a43fd32afaa8b834496
miracleXH <h1047741619@163.com>
2021-07-20 23:19:56 +0800

    Add object level encryption

---
+++ pkg/crypto/ole/ole_test.go
@@ -0,0 +1,41 @@
+package ole
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestDecrypt(t *testing.T) {
+	err := Setup("local")
+	require.NoError(t, err)
+
+	err = KeyGen([]string{"CompanyA", "Leader", "Male"}, "test1", "local")
+	require.NoError(t, err)
+
+	data := []byte("hello world!")
+	err = Encrypt(data, "(CompanyA and Male)",  "local", "input", "out")
+	require.NoError(t, err)
+
+	err = Decrypt("out.cpabe", "test1", "local", "plaintextFile1")
+	require.NoError(t, err)
+
+	plaintext, err := getPlainText("plaintextFile1")
+	require.NoError(t, err)
+	require.True(t, bytes.Equal(data, plaintext))
+}
+
+func TestInvalidDecrypt(t *testing.T) {
+	err := Setup("local")
+	require.NoError(t, err)
+
+	err = KeyGen([]string{"CompanyB", "Leader", "FeMale"}, "test2", "local")
+	require.NoError(t, err)
+
+	data := []byte("hello world!")
+	err = Encrypt(data, "(CompanyA and Male)",  "local", "input", "out")
+	require.NoError(t, err)
+
+	err = Decrypt("out.cpabe", "test2", "local", "plaintextFile1")
+	require.Error(t, err)
+}
