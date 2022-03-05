ac1c13e3846851a11743ceb2651e174e005158a4
ubuntu <myt_17@163.com>
2022-03-05 12:30:27 +0800

    delete KpabeUpdateThreshold()

--- pkg/crypto/delegate/fle/fle_delegate.go
+++ pkg/crypto/delegate/fle/fle_delegate.go
@@ -7,18 +7,6 @@ import (
 	kpabe "github.com/privacy-protection/kp-abe/delegate"
 )
 
-func KpabeUpdateThreshold(oldKey *kpabeProtos.Key, newThreshold []int) (*kpabeProtos.Key, error) {
-	threshold := make([]int32, len(newThreshold))
-	for i, num := range newThreshold {
-		threshold[i] = int32(num)
-	}
-	newKey, err := kpabe.UpdateThreshold(oldKey, threshold)
-	if err != nil {
-		return nil, fmt.Errorf("KpabeUpdateThreshold error, %v", err)
-	}
-	return kpabe.ReRandomizing(newKey)
-}
-
 func KpabeFieldDelegate(oldKey *kpabeProtos.Key, field []int) (*kpabeProtos.Key, error) {
 	newField := make([]int32, len(field))
 	for i, num := range field {
--- pkg/crypto/delegate/fle/fle_delegate_test.go
+++ pkg/crypto/delegate/fle/fle_delegate_test.go
@@ -9,29 +9,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestKpabeUpdateThreshold(t *testing.T) {
-	masterKey, err := fle.Setup()
-	require.NoError(t, err)
-
-	a := utils.Hash("pufa")
-	b := utils.Hash("zhaoshang")
-	c := utils.Hash("zhongzhai")
-	d := utils.Hash("jianshe")
-
-	key, err := fle.KeyGen(masterKey, []int{a, b, c, d})
-	require.NoError(t, err)
-
-	data := []byte("hello world")
-	ciphertext, err := fle.Encrypt(data, []int{a, b}, key.Param)
-	require.NoError(t, err)
-
-	key, err = KpabeUpdateThreshold(key, []int{2, 0, 0, 0, 0})
-
-	decodedData, err := fle.Decrypt(key, ciphertext)
-	require.NoError(t, err)
-	require.True(t, bytes.Equal(data, decodedData))
-}
-
 func TestKpabeFieldDelegate(t *testing.T) {
 	masterKey, err := fle.Setup()
 	require.NoError(t, err)
